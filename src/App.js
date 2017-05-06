78b26ff69ea39f1a90f41ce0308c6ed5a10f8ca8
matijastampfel <matija.stampfel@gmail.com>
2017-05-07 01:11:56 +0200

    v 0.02

---
+++ src/App.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+import React, { Component } from 'react';
+import './App.css';
+import TodoApp from './components/TodoApp.jsx';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <TodoApp/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import React, { Component } from 'react';
+
+class TodoApp extends Component {
+    render() {
+        return (
+            <div>
+            TodoApp.jsx    
+            </div>
+        );
+    }
+}
+
+export default TodoApp;
